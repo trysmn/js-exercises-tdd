7093a7d1ff5de39b9dffa471a7fd8c74c68f55b1
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-05-01 11:44:06 +0100

    Replace all vars with consts in test file

--- I.pass-tests/03-sum-numbers/add-numbers.test.js
+++ I.pass-tests/03-sum-numbers/add-numbers.test.js
@@ -1,10 +1,10 @@
-var addAllnumbers = require("./add-numbers");
+const addAllnumbers = require("./add-numbers");
 
 test("Add all numbers", function() {
-  var numbers = [9, 23, 10, 3, 8];
-  var expected = 53;
+  const numbers = [9, 23, 10, 3, 8];
+  const expected = 53;
 
-  var output = addAllnumbers(numbers);
+  const output = addAllnumbers(numbers);
 
   expect(output).toEqual(expected);
 });
