ac5b295f6028efe79fa5ca105c87e7a085eaf813
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-05-01 17:23:21 +0100

    Implement getAverageOfOnlyNumbers method with green tests

--- I.pass-tests/08-get-average/get-average.js
+++ I.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,18 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+function getAverageOfOnlyNumbers(arrayOfNumbersAndStrings) {
+    let numbersArray = [];
+    for (let arrayOfNumbersAndStringsIndex = 0; arrayOfNumbersAndStringsIndex < arrayOfNumbersAndStrings.length; arrayOfNumbersAndStringsIndex++) {
+        if (typeof arrayOfNumbersAndStrings[arrayOfNumbersAndStringsIndex] === 'number') {
+            numbersArray.push(arrayOfNumbersAndStrings[arrayOfNumbersAndStringsIndex]);
+        }
+    }
+    let sumOfNumbersInArray = 0;
+    for (let numbersArrayIndex = 0; numbersArrayIndex < numbersArray.length; numbersArrayIndex++) {
+        sumOfNumbersInArray += numbersArray[numbersArrayIndex];
+    }
+    return sumOfNumbersInArray/numbersArray.length;
+}
+
+module.exports = getAverageOfOnlyNumbers;
