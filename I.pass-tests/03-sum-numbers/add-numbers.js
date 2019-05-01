922fcc1970c18725e1a97dc92d34bbde10253ec3
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-05-01 11:47:54 +0100

    Implement sumAllNumbers method with green tests

--- I.pass-tests/03-sum-numbers/add-numbers.js
+++ I.pass-tests/03-sum-numbers/add-numbers.js
@@ -1,2 +1,9 @@
-function addNumbers(numbers) {}
+function addNumbers(numbersArray) {
+    let count = 0;
+    for (let numbersArrayIndex = 0; numbersArrayIndex < numbersArray.length; numbersArrayIndex++) {
+        count += numbersArray[numbersArrayIndex];
+    }
+    return count;
+}
+
 module.exports = addNumbers;
--- I.pass-tests/03-sum-numbers/add-numbers.test.js
+++ I.pass-tests/03-sum-numbers/add-numbers.test.js
@@ -1,10 +1,10 @@
-const addAllnumbers = require("./add-numbers");
+const addAllNumbers = require("./add-numbers");
 
 test("Add all numbers", function() {
   const numbers = [9, 23, 10, 3, 8];
   const expected = 53;
 
-  const output = addAllnumbers(numbers);
+  const output = addAllNumbers(numbers);
 
   expect(output).toEqual(expected);
 });
