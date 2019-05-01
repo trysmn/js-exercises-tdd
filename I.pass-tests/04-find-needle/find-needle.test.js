4198a557d6df86c8b4ed0926d7f40134672160a2
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-05-01 11:57:29 +0100

    Replace all vars with consts

--- I.pass-tests/04-find-needle/find-needle.test.js
+++ I.pass-tests/04-find-needle/find-needle.test.js
@@ -1,17 +1,17 @@
-var findTheNeedle = require("./find-needle");
+const findTheNeedle = require("./find-needle");
 
 test("Find the needle", function() {
-  var words = ["house", "train", "slide", "needle", "book"];
-  var expected = 3;
+  const words = ["house", "train", "slide", "needle", "book"];
+  const expected = 3;
 
-  var output = findTheNeedle(words, "needle");
+  const output = findTheNeedle(words, "needle");
   expect(output).toEqual(expected);
 });
 
 test("Find the plant", function() {
-  var words = ["plant", "shelf", "arrow", "bird"];
-  var expected = 0;
+  const words = ["plant", "shelf", "arrow", "bird"];
+  const expected = 0;
 
-  var output = findTheNeedle(words, "plant");
+  const output = findTheNeedle(words, "plant");
   expect(output).toEqual(expected);
 });
