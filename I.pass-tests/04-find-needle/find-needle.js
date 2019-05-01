6e5f5db8f96fd9307cbefc1085cfa0330bcead9c
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-05-01 12:04:49 +0100

    Implement findIndexOfElement method with green tests

--- I.pass-tests/04-find-needle/find-needle.js
+++ I.pass-tests/04-find-needle/find-needle.js
@@ -1,3 +1,9 @@
-function findNeedle(words) {}
+function findIndexOfElement(words, element) {
+    for (let wordsIndex = 0; wordsIndex < words.length; wordsIndex++) {
+        if (words[wordsIndex] === element) {
+            return wordsIndex;
+        }
+    }
+}
 
-module.exports = findNeedle;
+module.exports = findIndexOfElement;
