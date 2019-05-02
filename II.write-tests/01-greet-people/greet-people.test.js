9fe51bbcf642df4dc6338a430051deb51979dad5
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-05-02 10:08:37 +0100

    Write test for greetPeople function with a green test

--- II.write-tests/01-greet-people/greet-people.test.js
+++ II.write-tests/01-greet-people/greet-people.test.js
@@ -3,9 +3,11 @@ const greetPeople = require('./greet-people');
 test("print list of names prefixed with Hello", function() {
   // Arrange
   const mentors = ['Irina', 'Ashleigh', 'Etza'];
+  const expectedResult = "Hello IrinaAshleighEtza";
 
   // Act
   const result = greetPeople(mentors);
 
   // Assert
+  expect(result).toEqual(expectedResult);
 });
