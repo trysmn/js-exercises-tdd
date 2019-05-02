b2f537bee8cdc9d2a9c81f52f1b4fe4971dc7c88
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-05-02 10:03:34 +0100

    Commit changes to open another file

--- II.write-tests/01-greet-people/greet-people.test.js
+++ II.write-tests/01-greet-people/greet-people.test.js
@@ -1,5 +1,11 @@
+const greetPeople = require('./greet-people');
+
 test("print list of names prefixed with Hello", function() {
   // Arrange
+  const mentors = ['Irina', 'Ashleigh', 'Etza'];
+
   // Act
+  const result = greetPeople(mentors);
+
   // Assert
 });
