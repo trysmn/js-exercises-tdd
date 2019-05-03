7d4ca38f881e9c68a93b29e3665ec66c5711b4f0
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-05-03 10:47:42 +0100

    Write test for largerThanTen() method with a green test

--- II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.js
+++ II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.js
@@ -1,5 +1,7 @@
-module.exports = function(array) {
+const largerThanTen = function(array) {
   return array.filter(number => {
     return number > 10;
   });
 };
+
+module.exports = largerThanTen;
--- II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
+++ II.write-tests/04-numbers-greater-than-10/numbersGreaterThan10.test.js
@@ -1,6 +1,17 @@
-var largerThanTen = require("./numbersGreaterThan10");
+const largerThanTen = require("./numbersGreaterThan10");
 
-test("Get numbers greater than 10", function() {});
+test("Get numbers greater than 10", function() {
+    // Arrange
+    const inputArray = [4, 10, 32, 9, 21];
+    const expectedOutput = [32, 21];
+
+    // Call
+    const result = largerThanTen(inputArray);
+    console.log(result);
+
+    // Assert
+    expect(result).toEqual(expectedOutput);
+});
 
 // input: [4, 10, 32, 9, 21];
 // expected output: [32, 21];
