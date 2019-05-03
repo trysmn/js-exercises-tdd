ec30527effdff1aebb458740e575dcd3d9bf20b0
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-05-03 13:15:03 +0100

    Write test for getSecondAndThirdSmallest() method with a green test

--- II.write-tests/05-get-second-third-smallest/get-second-third.test.js
+++ II.write-tests/05-get-second-third-smallest/get-second-third.test.js
@@ -3,3 +3,17 @@
 // expected output = [6, 8];
 
 // also test that the original array has not changed
+
+const getSecondAndThirdSmallest = require('./get-second-third');
+
+test("returns second and third smallest values in the array, without changing it", () => {
+    // Arrange
+    const inputArray = [90, 5, 11, 8, 6];
+    const expectedOutput = [6, 8];
+
+    // Act
+    const result = getSecondAndThirdSmallest(inputArray);
+
+    // Assert
+    expect(result).toEqual(expectedOutput);
+});
