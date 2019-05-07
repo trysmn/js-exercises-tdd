57b5921e2ec53eb0c1a2b747b552d912c9cfda85
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-05-07 16:17:08 +0100

    Write failing test for case where input is not a String type and implement it with a green test and write failing test for case where input is valid and implement it with a green test

--- III.tdd-katas/calculator-tdd/string_calculator.js
+++ III.tdd-katas/calculator-tdd/string_calculator.js
@@ -1,6 +1,15 @@
 function stringCalculator(stringOfNumbers) {
     if (stringOfNumbers === "") {
         return 0;
+    } else if (typeof stringOfNumbers !== 'string') {
+        return Error("Invalid argument type");
+    } else {
+        const numbersArray = stringOfNumbers.split(",");
+        let count = 0;
+        numbersArray.map((element) => {
+            count += parseInt(element);
+        });
+        return count;
     }
 }
 
--- III.tdd-katas/calculator-tdd/string_calculator.test.js
+++ III.tdd-katas/calculator-tdd/string_calculator.test.js
@@ -11,5 +11,29 @@ describe("String Calculator", () => {
 
         // Assert
         expect(result).toEqual(expectedOutput);
+    }),
+
+    test("should return 0 given an invalid input type (not a string)", () => {
+        // Arrange
+        const inputString = 20;
+        const expectedOutput = Error("Invalid argument type");
+
+        // Act
+        const result = stringCalculator(inputString);
+
+        // Assert
+        expect(result).toEqual(expectedOutput);
+    }),
+
+    test("should return the sum of all numbers given in the input string", () => {
+        // Arrange
+        const inputString = "1,2,3";
+        const expectedOutput = 6;
+
+        // Act
+        const result = stringCalculator(inputString);
+
+        // Assert
+        expect(result).toEqual(expectedOutput);
     });
 });
