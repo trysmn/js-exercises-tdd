54fc8e2b25af02d91eed0308423f34d5b312bd34
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-05-07 15:34:08 +0100

    Read and understood how to mock functions in JEST, added a test in async-2.test.js that doesn't use a mocked version of fetcher

--- II.write-tests/09-test-async/async-2.test.js
+++ II.write-tests/09-test-async/async-2.test.js
@@ -5,7 +5,7 @@ jest.mock("./fetcher");
 test("gets a list of repositories names (with mock)", function() {
   // arrange
   fetcher.mockResolvedValue([{ name: "js-exercises" }]);
-  var url = "https://api.github.com/users/kabaros/repos";
+  const url = "https://api.github.com/users/kabaros/repos";
 
   // act
   return getRepos(url).then(function(result) {
@@ -14,4 +14,13 @@ test("gets a list of repositories names (with mock)", function() {
   });
 });
 
-test("a more deterministic test", function() {});
+test("gets a list of repositories names (without mock)", function() {
+  // Arrange
+  const url = "https://api.github.com/users/kabaros/repos";
+
+  // Act 
+  return getRepos(url).then(function(result) {
+    // Assert
+    expect(result).toContain("js-exercises");
+  });
+});
