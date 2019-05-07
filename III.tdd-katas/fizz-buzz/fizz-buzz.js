eed0c897c3e6379151ff52004958da61cb000b73
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-05-07 18:12:13 +0100

    Wrote tests for new requirements - printing Fizz when the counting integer contains the digit 3 and printing Buzz when the counting integer contains the digit 5

--- III.tdd-katas/fizz-buzz/fizz-buzz.js
+++ III.tdd-katas/fizz-buzz/fizz-buzz.js
@@ -3,9 +3,9 @@ function fizzBuzz(upperLimitInteger) {
     for (let countingInteger = 1; countingInteger <= upperLimitInteger; countingInteger++) {
         if (countingInteger % 3 === 0 && countingInteger % 5 === 0) {
             integerArray.push("FizzBuzz");
-        } else if (countingInteger % 3 === 0) {
+        } else if (countingInteger % 3 === 0 || String(countingInteger).includes("3")) {
             integerArray.push("Fizz");
-        } else if (countingInteger % 5 === 0) {
+        } else if (countingInteger % 5 === 0 || String(countingInteger).includes("5")) {
             integerArray.push("Buzz");
         } else {
             integerArray.push(countingInteger);
--- III.tdd-katas/fizz-buzz/fizz-buzz.test.js
+++ III.tdd-katas/fizz-buzz/fizz-buzz.test.js
@@ -31,10 +31,31 @@ describe("Fizz Buzz", () => {
     const result = fizzBuzz(upperLimitInteger);
 
     // Assert
-    expect(result).toEqual([1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz",13,14,"FizzBuzz"]);
-  })
-}
-);
+    expect(result).toEqual([1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz","Fizz",14,"FizzBuzz"]);
+  }),
+
+  test("should print Fizz if integer has the digit 3 in it, but is not divisible by 5", () => {
+    // Arrange
+    const upperLimitInteger = 13;
+
+    // Act
+    const result = fizzBuzz(upperLimitInteger);
+
+    // Assert
+    expect(result).toEqual([1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz","Fizz"]);
+  }),
+
+  test("should print Buzz if integer has the digit 5 in it, but is not divisible by 3", () => {
+    // Arrange
+    const upperLimitInteger = 25;
+
+    // Act
+    const result = fizzBuzz(upperLimitInteger);
+
+    // Assert
+    expect(result).toEqual([1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz","Fizz",14,"FizzBuzz",16,17,"Fizz",19,"Buzz","Fizz",22,"Fizz","Fizz","Buzz"]);
+  });
+});
 
 
 
