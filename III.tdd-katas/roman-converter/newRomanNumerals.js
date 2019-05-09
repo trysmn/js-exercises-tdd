17a41a2de2e48ebb655ae81527ac24345be2fc7a
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-05-09 11:00:11 +0100

    Refactored out logic for special digit position and what the special digit is

--- III.tdd-katas/roman-converter/newRomanNumerals.js
+++ III.tdd-katas/roman-converter/newRomanNumerals.js
@@ -5,23 +5,31 @@ function convertToNewRoman(integer) {
     let newStyleRomanNumeral;
     const integerDigitsArray = String(integer).split("");
     for (let integerDigitsArrayIndex = 0; integerDigitsArrayIndex < integerDigitsArray.length; integerDigitsArrayIndex++) {
-        if (integerDigitsArrayIndex === integerDigitsArray.length - 1 && integerDigitsArray[integerDigitsArrayIndex] === '4') {
+        if (isDigitInColumn(integerDigitsArrayIndex, integerDigitsArray.length - 1) && isDigitSpecial(integerDigitsArray[integerDigitsArrayIndex],'4')) {
             newStyleRomanNumeral = oldStyleRomanNumeral.replace("IIII","IV");
-        } else if (integerDigitsArrayIndex === integerDigitsArray.length - 1 && integerDigitsArray[integerDigitsArrayIndex] === '9') {
+        } else if (isDigitInColumn(integerDigitsArrayIndex, integerDigitsArray.length - 1) && isDigitSpecial(integerDigitsArray[integerDigitsArrayIndex],'9')) {
             newStyleRomanNumeral = oldStyleRomanNumeral.replace("VIIII","IX");
-        } else if (integerDigitsArrayIndex === integerDigitsArray.length - 2 && integerDigitsArray[integerDigitsArrayIndex] === '4') {
+        } else if (isDigitInColumn(integerDigitsArrayIndex, integerDigitsArray.length - 2) && isDigitSpecial(integerDigitsArray[integerDigitsArrayIndex],'4')) {
             newStyleRomanNumeral = oldStyleRomanNumeral.replace("XXXX","XL");
-        } else if (integerDigitsArrayIndex === integerDigitsArray.length - 2 && integerDigitsArray[integerDigitsArrayIndex] === '9') {
+        } else if (isDigitInColumn(integerDigitsArrayIndex, integerDigitsArray.length - 2) && isDigitSpecial(integerDigitsArray[integerDigitsArrayIndex],'9')) {
             newStyleRomanNumeral = oldStyleRomanNumeral.replace("LXXXX","XC");
-        } else if (integerDigitsArrayIndex === integerDigitsArray.length - 3 && integerDigitsArray[integerDigitsArrayIndex] === '4') {
+        } else if (isDigitInColumn(integerDigitsArrayIndex, integerDigitsArray.length - 3) && isDigitSpecial(integerDigitsArray[integerDigitsArrayIndex],'4')) {
             newStyleRomanNumeral = oldStyleRomanNumeral.replace("CCCC","CD");
-        } else if (integerDigitsArrayIndex === integerDigitsArray.length - 3 && integerDigitsArray[integerDigitsArrayIndex] === '9') {
+        } else if (isDigitInColumn(integerDigitsArrayIndex, integerDigitsArray.length - 3) && isDigitSpecial(integerDigitsArray[integerDigitsArrayIndex],'9')) {
             newStyleRomanNumeral = oldStyleRomanNumeral.replace("DCCCC","CM");
         }
     }
     return newStyleRomanNumeral;
 }
 
-console.log(convertToNewRoman(4));
+function isDigitInColumn(digitLocation, column) {
+    return digitLocation === column;
+}
+
+function isDigitSpecial(integer, specialInteger) {
+    return integer === specialInteger;
+}
+
+convertToNewRoman(4);
 
 module.exports = convertToNewRoman;
